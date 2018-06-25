dd7323e300309ea9058304b8e42310c9ce96ec85
Adams1991 <adams121@hotmail.co.uk>
2018-06-25 15:25:08 +0100

    water bottle done

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -18,7 +18,23 @@ public class WaterBottleTest {
         assertEquals(100, waterBottle.getVolume());
     }
 
+    @Test
+    public void canDrink(){
+        waterBottle.drink();
+        assertEquals(90, waterBottle.getVolume());
+    }
 
+    @Test
+    public void canEmpty(){
+        waterBottle.empty();
+        assertEquals(0,waterBottle.getVolume());
+    }
 
+   @Test
+    public void canFill(){
+        waterBottle.empty();
+        waterBottle.fill();
+        assertEquals(100,waterBottle.getVolume() );
+   }
 
 }
